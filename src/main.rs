781b7d5bec30cff47460a3551504e682d1f6e426
Samay Satardekar <mayursatardekar10@gmail.com>
2025-06-15 08:07:24 +0530

    Variables with Operations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let x = 5;
     println!("The value of x: {x}");
-    let x = x + 1;
+    let x = 8;
     println!("The value of x now is:{x}");
 
     // Adding the mut
@@ -9,4 +9,9 @@ fn main() {
     println!("The value of y is: {y}");
     y = 12;
     println!("The value of y now is using mut is:{y}");
+
+    let divi = 45.2 / 3.0;
+    let remaind = 50 % 3;
+    println!("The result of the division is: {divi}");
+    println!("The remainder is:{remaind}");
 }
