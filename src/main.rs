95668f951d6e4eb5bf4cc8e83d25659c0d918291
Samay Satardekar <mayursatardekar10@gmail.com>
2025-06-15 08:14:10 +0530

    Scalar Variables

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    //Scalar types - integer, float, boolean, character
     let x = 5;
     println!("The value of x: {x}");
     let x = 8;
@@ -10,8 +11,15 @@ fn main() {
     y = 12;
     println!("The value of y now is using mut is:{y}");
 
-    let divi = 45.2 / 3.0;
-    let remaind = 50 % 3;
+    let divi: f32 = 45.2 / 3.0;
+    let remaind: u8 = 50 % 3;
     println!("The result of the division is: {divi}");
     println!("The remainder is:{remaind}");
+
+    let boolt: bool = true;
+    let boolf: bool = false;
+    println!("These are the boolean values:{boolt} & {boolf}");
+
+    let char_new: char = 'S';
+    println!("This is the character value: {char_new}");
 }
